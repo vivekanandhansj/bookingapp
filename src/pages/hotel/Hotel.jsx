54edbff9d1ef66e4dda9294ddec1a99232af288e
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-12-09 03:20:22 +0000

    Update Hotel.jsx

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -26,7 +26,7 @@ const Hotel = () => {
   const [open, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
-  const { data, loading, error,address } = useFetch(`https://bookingapp-server.herokuapp.com/api/hotels/find/${id}`);
+  const { data, loading, error,address } = useFetch(`https://snappychat.vercel.app/api/hotels/find/${id}`);
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 

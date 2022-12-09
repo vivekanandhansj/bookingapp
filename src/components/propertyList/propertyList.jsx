8882f7f0a0f93337d8163648fa984b8271028ba7
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-12-09 03:13:24 +0000

    Update propertyList.jsx

--- src/components/propertyList/propertyList.jsx
+++ src/components/propertyList/propertyList.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
 
 const PropertyList = () => {
-  const { data, loading, error } = useFetch("https://bookingapp-server.herokuapp.com/api/hotels/countByType");
+  const { data, loading, error } = useFetch("https://snappychat.vercel.app/api/hotels/countByType");
   console.log(data)
 
   const images = [

1afb26239b5bda0a3a7b51a5cad429b4d4f71f8f
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-12-09 03:29:11 +0000

    Update List.jsx

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -18,7 +18,7 @@ const List = () => {
   const [max, setMax] = useState(undefined);
 
   const { data, loading, error, reFetch } = useFetch(
-    `https://bookingapp-server.herokuapp.com/api/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
+    `https://bookingapp-server.vercel.app/api/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
   );
 
   const handleClick = () => {

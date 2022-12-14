fac972cc63c00329f21a7a9ac854c6f9bd3122b9
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-12-14 10:49:17 +0530

    Update List.jsx

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -18,7 +18,7 @@ const List = () => {
   const [max, setMax] = useState(undefined);
 
   const { data, loading, error, reFetch } = useFetch(
-    `https://bookingapp-server-86sw59pxz-vivekanandhansj.vercel.app/api/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
+    `https://bookingapp-server.onrender.com/api/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
   );
 
   const handleClick = () => {

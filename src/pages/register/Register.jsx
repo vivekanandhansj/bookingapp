3ec0ae53f995ae380d85db0425282598d53bfd20
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-12-14 10:50:59 +0530

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("https://bookingapp-server-86sw59pxz-vivekanandhansj.vercel.app/api/auth/register", credentials);
+      const res = await axios.post("https://bookingapp-server.onrender.com/api/auth/register", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       navigate("/login")
     } catch (err) {

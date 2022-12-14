8f07c507bf796d0e89368a08a5e493b2ce7402ae
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-12-14 10:48:17 +0530

    Update Hotel.jsx

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -26,7 +26,7 @@ const Hotel = () => {
   const [open, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
-  const { data, loading, error,address } = useFetch(`https://bookingapp-server-86sw59pxz-vivekanandhansj.vercel.app/api/hotels/find/${id}`);
+  const { data, loading, error,address } = useFetch(`https://bookingapp-server.onrender.com/api/hotels/find/${id}`);
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
